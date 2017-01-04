139181d3697fa17131de251a65956fd835de0955
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-03 16:15:10 -0800

    Mock the player for failures

--- game.py
+++ game.py
@@ -5,15 +5,21 @@ name = raw_input("What is your name: ")
 print("%s, I'm thinking of a number between 1 and 100.") % name
 print("Try to guess my number.")
 number = random.randint(1, 100)
+print number
 guess = None
 num_guess = 0
 
 while number != guess:
-    guess = int(raw_input("What is your guess: "))
-    num_guess += 1
-    if guess > number:
-        print "Your guess is too high. Try again."
-    if guess < number:
-        print "Your guess is too low. Try again"
+    try:
+        guess = int(raw_input("What is your guess: "))
+        num_guess += 1
+        if (guess > 100 or guess < 1):
+            print "Your number is not what we asked for! Shame!"
+        elif guess > number:
+            print "Your guess is too high. Try again."
+        elif guess < number:
+            print "Your guess is too low. Try again"
+    except:
+        print "That's not even a number. Dishonor on you!"
 
 print("Well done, %s! You found my number in %d tries.") % (name, num_guess)
