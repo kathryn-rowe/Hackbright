96ba64c3ed08abc7599ca37c15c31cd35cadfae5
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-04 18:01:13 -0800

    Added many try-except conditions. It's almost fail-proof.

--- calculator.py
+++ calculator.py
@@ -11,6 +11,8 @@ from arithmetic import *
 # Your code goes here
 
 def parse_expression():
+    """ Creating a calcuator to create output based on prefix notation"""
+
     while True:
         expression = raw_input("> ")
         tokens = expression.split(" ")
@@ -22,30 +24,57 @@ def parse_expression():
         if first_token == 'q':
             return
 
-        if len(tokens) == 3:
+        if len(tokens) == 2:
+            try:
+                first_number = int(tokens[1])
 
-            first_number = int(tokens[1])
-            second_number = int(tokens[2])
+                if first_token == "square":
+                    print square(first_number)
+                elif first_token == "cube":
+                    print cube(first_number)
+                else:
+                    print "Are you trying to square or cube? Try again."
 
-            if first_token == "+":
-                print add(first_number, second_number)
-            elif first_token == "-":
-                print subtract(first_number, second_number)
-            elif first_token == "*":
-                print multiply(first_number, second_number)
-            elif first_token == "/":
-                print divide(first_number, second_number)
-            elif first_token == "square":
-                print square(first_number, second_number)
-            elif first_token == "cube":
-                print cube(first_number, second_number)
-            elif first_token == "pow":
-                print power(first_number, second_number)
-            elif first_token == "mod":
-                print mod(first_number, second_number)
-            else:
-                print "That is not a valid operator!"
+            except ValueError:
+                print "That was not a valid input"
+            except TypeError:
+                print "We want you to enter an operator and two numbers."
+            except IndexError:
+                print "We want you to enter an operator and two numbers."
+
         else:
-            print "That was not a valid input."
+            try:
+                first_number = int(tokens[1])
+                second_number = int(tokens[2])
+
+                if first_token == "+":
+                    print add(first_number, second_number)
+                elif first_token == "-":
+                    print subtract(first_number, second_number)
+                elif first_token == "*":
+                    print multiply(first_number, second_number)
+                elif first_token == "/":
+                    print divide(first_number, second_number)
+                elif first_token == "square":
+                    print square(first_number)
+                elif first_token == "cube":
+                    print cube(first_number)
+                elif first_token == "pow":
+                    print power(first_number, second_number)
+                elif first_token == "mod":
+                    print mod(first_number, second_number)
+                else:
+                    print "That is not a valid operator!"
+            #else:
+            #    print "That was not a valid input."
+
+            except ValueError:
+                print "That was not a valid input"
+            except TypeError:
+                print "We want you to enter an operator and two numbers."
+            except IndexError:
+                print "We want you to enter an operator and two numbers."
+
+
 
 parse_expression()
