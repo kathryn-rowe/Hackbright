19f77d2e0877bb73266fc4ccd12b236a3decdfb7
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-04 17:15:52 -0800

    Created while loop and finished all validation

--- calculator.py
+++ calculator.py
@@ -11,39 +11,41 @@ from arithmetic import *
 # Your code goes here
 
 def parse_expression():
-    expression = raw_input("> ")
-    tokens = expression.split(" ")
-#    print tokens
-#    print len(tokens)   
-    if len(tokens) != 1 and len(tokens) != 3:
-        print "That was not a valid input."
-        return
+    while True:
+        expression = raw_input("> ")
+        tokens = expression.split(" ")
+    #    print tokens
+    #    print len(tokens)
 
-    first_token = tokens[0]
+        first_token = tokens[0]
 
-    if first_token == 'q':
-        return
+        if first_token == 'q':
+            return
 
-    first_number = int(tokens[1])
-    second_number = int(tokens[2])
+        if len(tokens) == 3:
 
-    if first_token == "+":
-        print add(first_number, second_number)
-    elif first_token == "-":
-        print subtract(first_number, second_number)
-    elif first_token == "*":
-        print multiply(first_number, second_number)
-    elif first_token == "/":
-        print divide(first_number, second_number)
-    elif first_token == "square":
-        print square(first_number, second_number)
-    elif first_token == "cube":
-        print cube(first_number, second_number)
-    elif first_token == "pow":
-        print power(first_number, second_number)
-    elif first_token == "mod":
-        print mod(first_number, second_number)
-    else:
-        print "That was not a valid input."
+            first_number = int(tokens[1])
+            second_number = int(tokens[2])
+
+            if first_token == "+":
+                print add(first_number, second_number)
+            elif first_token == "-":
+                print subtract(first_number, second_number)
+            elif first_token == "*":
+                print multiply(first_number, second_number)
+            elif first_token == "/":
+                print divide(first_number, second_number)
+            elif first_token == "square":
+                print square(first_number, second_number)
+            elif first_token == "cube":
+                print cube(first_number, second_number)
+            elif first_token == "pow":
+                print power(first_number, second_number)
+            elif first_token == "mod":
+                print mod(first_number, second_number)
+            else:
+                print "That is not a valid operator!"
+        else:
+            print "That was not a valid input."
 
 parse_expression()
