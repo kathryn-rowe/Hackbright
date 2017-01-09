b9bca40039dd3287a51276409d0867ffd58f5e89
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-09 03:24:35 +0000

    Deleted extra instructions

--- assessment.py
+++ assessment.py
@@ -1,6 +1,4 @@
 # PART ONE
-#    (a) Write a function that takes a town name as a string and evaluates to
-#        `True` if it is your hometown, and `False` otherwise.
 
 
 def is_hometown(town_name):
@@ -17,10 +15,6 @@ def is_hometown(town_name):
         #could also have a print statement say 'this isn't Kate's hometown'
 
 
-#    (b) Write a function that takes a first and last name as arguments and
-#        returns the concatenation of the two names in one string.
-
-
 def combine_first_last_name(first_name, last_name):
     """Takes two names, first and last, as arguments and returns them together
     as one string. """
@@ -29,20 +23,13 @@ def combine_first_last_name(first_name, last_name):
     #added the string function in case someone has a number in their name.
 
 
-#    (c) Write a function that takes a home town, a first name, and a last name
-#        as arguments, calls both functions from part (a) and (b) and prints
-#        "Hi, 'full name here', we're from the same place!", or "Hi 'full name
-#        here', where are you from?" depending on what the function from part
-#        (a) evaluates to.
-
-
 def learn_persons_name_hometown(hometown, first_name, last_name):
     """Finding the hometown, first name and last name of someone.
 
     Combine all these attributes, call previous functions ()"""
 
     name_combine = combine_first_last_name(first_name, last_name)
-    #takes information from previously created function creating the combo of 
+    #takes information from previously created function creating the combo of
     #input first and last names. This variable makes the if statement cleaner below.
 
     if is_hometown(hometown):
@@ -56,10 +43,6 @@ def learn_persons_name_hometown(hometown, first_name, last_name):
 ###############################################################################
 
 # PART TWO
-
-# 1. (a) Write a function, `is_berry()`, which takes a fruit name as a string
-#        and returns a boolean if the fruit is a "strawberry", "cherry", or
-#        "blackberry."
 
 
 def is_berry(fruit):
@@ -75,11 +58,6 @@ def is_berry(fruit):
         return False
 
 
-# (b) Write another function, shipping_cost(), which calculates shipping cost
-#     by taking a fruit name as a string and calling the is_berry() function
-#     within the shipping_cost() function. Your function should return 0 if
-#     is_berry() == True, and 5 if is_berry() == False.
-
 def shipping_cost(fruit):
     """Calculates shipping cost of fruit"""
 
@@ -87,11 +65,6 @@ def shipping_cost(fruit):
         return 0
     else:
         return 5
-
-
-# 2. Make a function that takes in a number and a list of numbers. It should
-#    return a new list containing the elements of the input list, along with
-#    given number, which should be at the end of the new list.
 
 
 def append_to_list(lst, num):
@@ -109,22 +82,6 @@ def append_to_list(lst, num):
     new_list.append(num)
 
     return new_list
-
-# 3. Write a function calculate_price to calculate an item's total cost by
-#    adding tax, and any fees required by state law.
-
-#    Your function will take as parameters (in this order): the base price of
-#    the item, a two-letter state abbreviation, and the tax percentage (as a
-#    two-digit decimal, so, for instance, 5% will be .05). If the user does not
-#    provide a tax rate it should default to 5%.
-
-#    CA law requires stores to collect a 3% recycling fee, PA requires a $2
-#    highway safety fee, and in MA, there is a commonwealth fund fee of $1 for
-#    items with a base price under $100 and $3 for items $100 or more. Fees are
-#    added *after* the tax is calculated.
-
-#    Your function should return the total cost of the item, including tax and
-#    fees.
 
 
 def calculate_price(item_price, state_abbrev, tax_percent=0.05):
@@ -229,16 +186,6 @@ def calculate_price(item_price, state_abbrev, tax_percent=0.05):
 
 # PART THREE: ADVANCED
 
-# NOTE: We haven't given you function signatures and docstrings for these, so
-# you'll need to write your own. These functions also aren't included in the
-# doctests above, so make sure to test them on your own.
-
-
-# 1. Make a new function that takes in a list and any number of additional
-# arguments, appends them to the list, and returns the entire list. Hint: this
-# isn't something we've discussed yet in class; you might need to google how to
-# write a Python function that takes in an arbitrary number of arguments.
-
 
 def append_arguments_to_list(input_list, *args):
     """This function takes in a list and any number of additional number of
@@ -254,18 +201,6 @@ def append_arguments_to_list(input_list, *args):
         #iterate over the unknown amount of arguments and add each one to the new list.
         new_list.append(argument)
     return new_list
-
-# 2. Make a new function with a nested inner function.
-# The outer function will take in a word.
-# The inner function will multiply that word by 3.
-# Then, the outer function will call the inner function.
-# Output will be the original function argument and the result of the inner
-# function.
-
-# Example:
-
-#>>> outer("Balloonicorn")
-#('Balloonicorn', 'BalloonicornBalloonicornBalloonicorn')
 
 
 def get_word(word):
