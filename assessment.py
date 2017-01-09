d388756081d60a0ee9fbd6d3d1e3819e78e630db
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-09 03:17:58 +0000

    Cleaned up code, added more comments

--- assessment.py
+++ assessment.py
@@ -1,61 +1,4 @@
-"""
-Skills function assessment.
-
-Please read the the instructions first (separate file). Your solutions should
-go below this docstring.
-
-PART ONE: Write your own function declarations - Part 1 questions aren't
-included in the doctest.
-
-PART TWO:
-
-    >>> is_berry("blackberry")
-    True
-
-    >>> is_berry("durian")
-    False
-
-    >>> shipping_cost("blackberry")
-    0
-
-    >>> shipping_cost("durian")
-    5
-
-    >>> append_to_list([3, 5, 7], 2)
-    [3, 5, 7, 2]
-
-    >>> calculate_price(25, "CA")
-    27.0
-
-    >>> calculate_price(400, "NM")
-    420.0
-
-    >>> calculate_price(150, "OR", 0)
-    150
-
-    >>> calculate_price(60, "PA")
-    65.0
-
-    >>> calculate_price(38, "MA")
-    40.9
-
-    >>> calculate_price(126, "MA")
-    135.3
-
-PART THREE: Write your own function declarations - Part 3 questions aren't
-included in the doctest.
-
-"""
-
-###############################################################################
-
 # PART ONE
-
-# NOTE: We haven't given you function signatures or docstrings for these, so
-# you'll need to write your own. These functions also aren't included in the
-# doctests above, so make sure to test them on your own.
-
-
 #    (a) Write a function that takes a town name as a string and evaluates to
 #        `True` if it is your hometown, and `False` otherwise.
 
@@ -66,10 +9,12 @@ def is_hometown(town_name):
 
     If the argument is Luck, then it is true. If it is not Luck, then it is false"""
 
-    if town_name == "Luck" or town_name == "luck":
+    if str(town_name) == "Luck" or str(town_name) == "luck":
+        #made the input town_name a string in case someone adds a number.
         return True
     else:
         return False
+        #could also have a print statement say 'this isn't Kate's hometown'
 
 
 #    (b) Write a function that takes a first and last name as arguments and
@@ -80,7 +25,7 @@ def combine_first_last_name(first_name, last_name):
     """Takes two names, first and last, as arguments and returns them together
     as one string. """
 
-    print str(first_name) + " " + str(last_name)
+    return str(first_name) + " " + str(last_name)
     #added the string function in case someone has a number in their name.
 
 
@@ -96,6 +41,17 @@ def learn_persons_name_hometown(hometown, first_name, last_name):
 
     Combine all these attributes, call previous functions ()"""
 
+    name_combine = combine_first_last_name(first_name, last_name)
+    #takes information from previously created function creating the combo of 
+    #input first and last names. This variable makes the if statement cleaner below.
+
+    if is_hometown(hometown):
+        #finds if the function returns to True, therefore same hometown
+        print "Hi, " + name_combine + ", we're from the same place!"
+    else:
+        print "Hi, " + name_combine + ", where are you from?"
+
+
 
 ###############################################################################
 
@@ -109,7 +65,11 @@ def learn_persons_name_hometown(hometown, first_name, last_name):
 def is_berry(fruit):
     """Determines if fruit is a berry"""
 
-    if fruit == "strawberry" or fruit == "cherry" or fruit == "blackberry":
+    fruit_lower = fruit.lower()
+    #this was something extra I added to make sure the input argument will return
+    #true if the correct berry was input into the statement below, regardless of cases.
+
+    if fruit_lower == "strawberry" or fruit_lower == "cherry" or fruit_lower == "blackberry":
         return True
     else:
         return False
@@ -129,7 +89,6 @@ def shipping_cost(fruit):
         return 5
 
 
-
 # 2. Make a function that takes in a number and a list of numbers. It should
 #    return a new list containing the elements of the input list, along with
 #    given number, which should be at the end of the new list.
@@ -140,14 +99,16 @@ def append_to_list(lst, num):
        added to the end."""
 
     new_list = []
+    #create empty list to add values. New_list isn't very creative, but it is direct.
+
     for number in lst:
+        #again, variables aren't very creative but they make sense and are better
+        #than x,y or banana.
         new_list.append(number)
 
     new_list.append(num)
 
     return new_list
-
-
 
 # 3. Write a function calculate_price to calculate an item's total cost by
 #    adding tax, and any fees required by state law.
@@ -173,26 +134,35 @@ def calculate_price(item_price, state_abbrev, tax_percent=0.05):
     Find tax by multipling rate to the item price and then adding that amount
     to the item price. Find the fee by state abbreviation. Each state has its
     own fee codes. Function below uses fee examples given in directions. Total
-    calculated price includes tax and state fee."""
+    calculated price includes tax and state fee.
+
+    Correct tax rates are dependent on argument input. If I was to continue
+    writing this code, I would find a text document of all state codes and
+    open file, loop through records, and input correct values. Right now I have
+    no way of knowing (other than looking for each state rate online) if the user
+    input is correct and if the default of 5%  applies to states. """
 
     if state_abbrev == "CA" or state_abbrev == "ca":
-    #state abbreviation is suppose to be a two-letter input. Could include a
-    #try-except statement to catch inputs longer than two letters.
+    #CALIFORNIA: state abbreviation is suppose to be a two-letter input.
+    #Could include a try-except statement to catch inputs longer than two letters.
 
         ca_fee = .03
 
         if tax_percent != .05:
+            #find the price with tax, then multiple by CA's 3% fee
             price_with_tax = (item_price * (tax_percent / 100)) + item_price
             total_price = (price_with_tax * ca_fee) + price_with_tax
             return round(total_price, 1)
+            #rounded values to not have more than 2 decimal points.
         else:
             price_with_tax = ((item_price * .05) + item_price)
             total_price = (price_with_tax * ca_fee) + price_with_tax
             return round(total_price, 1)
+            #rounded values to not have more than 2 decimal points.
 
     elif state_abbrev == "PA" or state_abbrev == "pa":
-    #state abbreviation is suppose to be a two-letter input. Could include a
-    #try-except statement to catch inputs longer than two letters.
+    #PENNSYLVANIA: state abbreviation is suppose to be a two-letter input.
+    #Could include a try-except statement to catch inputs longer than two letters.
 
         pa_fee = 2.0
 
@@ -200,39 +170,60 @@ def calculate_price(item_price, state_abbrev, tax_percent=0.05):
             price_with_tax = (item_price * (tax_percent / 100)) + item_price
             total_price = price_with_tax + pa_fee
             return round(total_price, 1)
+            #rounded values to not have more than 2 decimal points.
+
         else:
+            #if the tax argument is forgetten, 5% is calculated.
             total_price = ((item_price * 0.05) + item_price) + pa_fee
             return round(total_price, 1)
+            #rounded values to not have more than 2 decimal points.
 
     elif state_abbrev == "MA" or state_abbrev == "ma":
-    #state abbreviation is suppose to be a two-letter input. Could include a
-    #try-except statement to catch inputs longer than two letters.
+    #MASSACHUSETTS: state abbreviation is suppose to be a two-letter input.
+    #Could include a try-except statement to catch inputs longer than two letters.
 
         if item_price < 100:
+            #MA fee depends on if item is more or less than $100
+
             ma_fee_less_100 = 1.0
+
             if tax_percent != 0.05:
                 price_with_tax = (item_price * (tax_percent / 100)) + item_price
                 total_price = price_with_tax + ma_fee_less_100
                 return round(total_price, 1)
+                #rounded values to not have more than 2 decimal points.
+
             else:
+                #if the tax argument is forgetten, 5% is calculated.
                 total_price = ((item_price * 0.05) + item_price) + ma_fee_less_100
                 return round(total_price, 1)
+                #rounded values to not have more than 2 decimal points.
         else:
+
             ma_fee_more_100 = 3.0
+
             if tax_percent != 0.05:
                 price_with_tax = (item_price * (tax_percent / 100)) + item_price
                 total_price = price_with_tax + ma_fee_more_100
                 return round(total_price, 1)
+                #rounded values to not have more than 2 decimal points.
+
             else:
+                #if the tax argument is forgetten, 5% is calculated.
                 total_price = ((item_price * 0.05) + item_price) + ma_fee_more_100
                 return round(total_price, 1)
+                #rounded values to not have more than 2 decimal points.
+
     else:
+        #to find total price of item in states other than CA, PA, MA
         if tax_percent != 0.05:
             price_with_tax = (item_price * (tax_percent / 100)) + item_price
             return price_with_tax
+
         else:
             total_price = (item_price * 0.05) + item_price
             return round(total_price, 1)
+            #rounded values to not have more than 2 decimal points.
 
 ###############################################################################
 
@@ -283,7 +274,9 @@ def get_word(word):
     def mult_by_three(argument):
         """Multiples argument by 3, no spacing"""
 
-        return argument * 3
+        return str(argument) * 3
+        #made the argument a string in case a number is added, therefore the output
+        #would be a value/one number
 
     #returns the word used as the argument and a non-spaced version of that word.
     return word, mult_by_three(word)
