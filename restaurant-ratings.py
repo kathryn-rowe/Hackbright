92e60d993d90a22a03ce4fe6de463cf4d68936d8
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-09 17:59:46 -0800

    Adding new option to update selected restraurant' rating

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,6 +1,7 @@
 # your code goes here
 import random
 
+
 def choose_adventure(file_name):
     """Allow user to choose to see ratings in alphabetical order, add a
     new restaurant, or quit"""
@@ -9,20 +10,22 @@ def choose_adventure(file_name):
         user_choice = raw_input("""Would you like to \n
         1. See the restaurant list alphabetically,\n
         2. Add a new restaurant to the list (and see the list), or\n
-        3. Update an existing restaurant' s rating, \n
+        3. Update an existing restaurant's rating, \n
         4. Quit?\n
-        Enter 1, 2, 3 or 4: """)
+        Enter 1, 2, 3 or 4:\n
+        >>>\n """)
 
         if user_choice == "1":
-            sort_restaurant_by_name(file_name, False, False)
+            sort_restaurant_by_name(file_name, "")
         elif user_choice == "2":
-            sort_restaurant_by_name(file_name, True, False)
+            sort_restaurant_by_name(file_name, "adding_new")
         elif user_choice == "3":
-            sort_restaurant_by_name(file_name, False, True)
+            sort_restaurant_by_name(file_name, "updating_rating")
         elif user_choice == "4":
             return
         else:
             print "You did not choose 1, 2, 3 or 4."
+
 
 def collect_new_restaurant():
     """Collect new reataurant name and score."""
@@ -39,17 +42,41 @@ def collect_new_restaurant():
 
     return user_restaurant, user_restaurant_score
 
+
 def updating_restaurant(dic):
-    random_restaurant_name = random.choice(dic.keys())
-    random_restaurant_rating = dic[random_restaurant_name]
-    print random_restaurant_rating
-    print "The random restaurant is %s and its rating score is %s" % (random_restaurant_name, random_restaurant_rating)
-    new_rating = int(raw_input("What is the new rating? "))
+    """Updating a random restaurant rating score"""
+    updating_option = raw_input("""Would you like to\n
+    1. Choose the restaurant to update, or \n
+    2. Let us choose a random one?\n
+    Please enter 1 or 2:  >>> \n""")
+
+    if updating_option == "1":
+        restaurant_name = raw_input("""Options: Big Bean Shack, Chip shop, Diagon Alley cafe,\n
+            Eternelle's Elixir of Refreshment, Florean Fortescue's Ice Cream Parlour, Jellied Eel Shop,\n
+            Luchino Caffe, Ministry Munchies, The Bear & Staff, The Club, The Porcupine, or The Tavern.\n
+            Restaurant name: """)
+        restaurant_rating = dic[restaurant_name]
+
+    else:
+        restaurant_name = random.choice(dic.keys())
+        restaurant_rating = dic[restaurant_name]
+
+    print "The random restaurant is %s and its rating score is %s" % (restaurant_name, restaurant_rating)
+
+    while True:
+        try:
+            new_rating = int(raw_input("What is the new rating? "))
+            break
+        except Exception:
+            print "Please enter a number between 1 - 5."
+
     dic[random_restaurant_name] = new_rating
+
     return dic
 
-def sort_restaurant_by_name(file_name, adding_new, updating_rating):
-    """Sort restaurant by name"""
+
+def sort_restaurant_by_name(file_name, options):
+    """Create dictionary and modify the dictionary accordig to user chosen option."""
 
     the_file = open(file_name)
 
@@ -59,11 +86,11 @@ def sort_restaurant_by_name(file_name, adding_new, updating_rating):
         key, value = line.rstrip().split(":")
         dic[key] = value
 
-    if adding_new:
-        [user_restaurant, user_restaurant_score] = collect_new_restaurant()
+    if options == "adding_new":
+        user_restaurant, user_restaurant_score = collect_new_restaurant()
         dic[user_restaurant] = user_restaurant_score
 
-    if updating_rating:
+    if options == "updating_rating":
         dic = updating_restaurant(dic)
 
     sorted_key = sorted(dic)
