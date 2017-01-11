895c01674cba17183d1a1a6ea54896cfcc53844f
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-11 13:04:15 -0800

    Can change how many words in n_gram, almost working

--- markov.py
+++ markov.py
@@ -1,4 +1,5 @@
 from random import choice
+import sys
 
 
 def open_and_read_file(file_path):
@@ -7,11 +8,10 @@ def open_and_read_file(file_path):
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
-
+    #file_path = sys.argv[1]
     input_text = open(file_path).read()
 
     return input_text
-
 
 def make_chains(text_string):
     """Takes input text as string; returns _dictionary_ of markov chains.
@@ -28,12 +28,17 @@ def make_chains(text_string):
     words = text_string.split()
 
     chains = {}
+    #this finds how many words to put in the n_gram tuple
+    n_gram = int(raw_input("How many words do you want in your n_gram?: "))
 
-    for i in range(len(words) - 2):
-        tuple_bi_gram = words[i], words[i + 1]
-        following_word = words[i + 2]
-        chains[tuple_bi_gram] = chains.get(tuple_bi_gram, [])
-        chains[tuple_bi_gram].append(following_word)
+    tuple_n_gram = tuple(words[:n_gram])
+
+    print tuple_n_gram
+
+    for i in range(len(words) - n_gram):
+        following_word = words[i + n_gram]
+        chains[tuple_n_gram] = chains.get(tuple_n_gram, [])
+        chains[tuple_n_gram].append(following_word)
 
     return chains
 
@@ -44,8 +49,9 @@ def make_text(chains):
     key = choice(chains.keys())
 
     text = " ".join(key)
-
+    #need to change this to coorespond with how many words are going in the n_gram tuple.
     while key in chains:
+        word_one = key[0]
         word_two = key[1]
         word_three = choice(chains[key])
         text += " " + word_three
@@ -53,15 +59,14 @@ def make_text(chains):
 
     # import pprint
     # pprint.pprint(chains)
-    print text
-
-    #return text
+    return text
 
 
-input_path = "green-eggs.txt"
+
+file_path = sys.argv[1]
 
 # Open the file and turn it into one long string
-input_text = open_and_read_file(input_path)
+input_text = open_and_read_file(file_path)
 
 # Get a Markov chain
 chains = make_chains(input_text)
