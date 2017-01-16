125d7c8ac0984549fd75a02d0072c19b2cd50086
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-16 03:59:23 +0000

    All working but one.

--- assessment.py
+++ assessment.py
@@ -70,37 +70,6 @@ def translate_to_pirate_talk(phrase):
     Given a phrase, translate each word to the Pirate-speak
     equivalent. Words that cannot be translated into Pirate-speak
     should pass through unchanged. Return the resulting sentence.
-
-    Here's a table of English to Pirate translations:
-
-    ----------  ----------------
-    English     Pirate
-    ----------  ----------------
-    sir         matey
-    hotel       fleabag inn
-    student     swabbie
-    man         matey
-    professor   foul blaggart
-    restaurant  galley
-    your        yer
-    excuse      arr
-    students    swabbies
-    are         be
-    restroom    head
-    my          me
-    is          be
-    ----------  ----------------
-
-    For example::
-
-        >>> translate_to_pirate_talk("my student is not a man")
-        'me swabbie be not a matey'
-
-    You should treat words with punctuation as if they were different
-    words::
-
-        >>> translate_to_pirate_talk("my student is not a man!")
-        'me swabbie be not a man!'
     """
     pirate_dictionary = {"sir": "matey",
                          "hotel": "fleabag inn",
@@ -115,6 +84,7 @@ def translate_to_pirate_talk(phrase):
                          "restroom": "head",
                          "my": "me",
                          "is": "be"}
+    # Seperate string into seperate words.
     word_line = ""
 
     for item in phrase:
@@ -122,6 +92,8 @@ def translate_to_pirate_talk(phrase):
 
     word_line = word_line.split()
 
+    # Iterate through words to see if they are in Pirate Dictionary. If in dictionary
+    # change word to pirate word, otherwise keep word in sentence.
     pirate_sentence = ""
 
     for word in word_line:
@@ -132,54 +104,86 @@ def translate_to_pirate_talk(phrase):
 
     return pirate_sentence.rstrip()
 
+# NOTE: This is a function I had to comment out. I have many questions about it
+# and could not get it to work.
+# def kids_game(names):
+#     """Play a kids' word chain game.
 
-def kids_game(names):
-    """Play a kids' word chain game.
+#     Given a list of names, like::
 
-    Given a list of names, like::
+#       bagon baltoy yamask starly nosepass kalob nicky
 
-      bagon baltoy yamask starly nosepass kalob nicky
+#     Do the following:
 
-    Do the following:
+#     1. Always start with the first word ("bagon", in this example).
 
-    1. Always start with the first word ("bagon", in this example).
+#     2. Add it to the results.
 
-    2. Add it to the results.
+#     3. Use the last letter of that word to look for the next word.
+#        Since "bagon" ends with n, find the *first* word starting
+#        with "n" in our list --- in this case, "nosepass".
 
-    3. Use the last letter of that word to look for the next word.
-       Since "bagon" ends with n, find the *first* word starting
-       with "n" in our list --- in this case, "nosepass".
+#     4. Add "nosepass" to the results, and continue. Once a word has
+#        been used, it can't be used again --- so we'll never get to
+#        use "bagon" or "nosepass" a second time.
 
-    4. Add "nosepass" to the results, and continue. Once a word has
-       been used, it can't be used again --- so we'll never get to
-       use "bagon" or "nosepass" a second time.
+#     5. When you can't find an unused word to use, you're done!
+#        Return the list of output words.
 
-    5. When you can't find an unused word to use, you're done!
-       Return the list of output words.
+#     For example::
 
-    For example::
+#         >>> kids_game(["bagon", "baltoy", "yamask", "starly",
+#         ...            "nosepass", "kalob", "nicky", "booger"])
+#         ['bagon', 'nosepass', 'starly', 'yamask', 'kalob', 'baltoy']
 
-        >>> kids_game(["bagon", "baltoy", "yamask", "starly",
-        ...            "nosepass", "kalob", "nicky", "booger"])
-        ['bagon', 'nosepass', 'starly', 'yamask', 'kalob', 'baltoy']
+#     (After "baltoy", there are no more y-words, so we end, even
+#     though "nicky" and "booger" weren't used.)
 
-    (After "baltoy", there are no more y-words, so we end, even
-    though "nicky" and "booger" weren't used.)
+#     Two more examples:
 
-    Two more examples:
+#         >>> kids_game(["apple", "berry", "cherry"])
+#         ['apple']
 
-        >>> kids_game(["apple", "berry", "cherry"])
-        ['apple']
+#         >>> kids_game(["noon", "naan", "nun"])
+#         ['noon', 'naan', 'nun']
 
-        >>> kids_game(["noon", "naan", "nun"])
-        ['noon', 'naan', 'nun']
+#     This is a tricky problem. In particular, think about how using
+#     a dictionary (with the super-fast lookup they provide) can help;
+#     good solutions here will definitely require a dictionary.
+#     """
+#     # name_dictionary = {}
 
-    This is a tricky problem. In particular, think about how using
-    a dictionary (with the super-fast lookup they provide) can help;
-    good solutions here will definitely require a dictionary.
-    """
+#     # value = 1
 
-    return []
+#     # for name in names:
+#     #     name_dictionary[value] = name
+#     #     value += 1
+
+#     # word_list = []
+
+#     # first_word = name_dictionary.get(1)
+#     # word_list.append(first_word)
+#     # last_letter = first_word[-1]
+#     # del name_dictionary[1]
+
+
+#     # for key, value in name_dictionary.items():
+#     #     if value.startswith(last_letter):
+#     #         last_letter = value[-1]
+#     #         word_list.append(value)
+#     #         del name_dictionary[key]
+#     # for key, value in name_dictionary.items():
+#     #     if value.startswith(last_letter):
+#     #         last_letter = value[-1]
+#     #         word_list.append(value)
+#     #         del name_dictionary[key]
+#     # for key, value in name_dictionary.items():
+#     #     if value.startswith(last_letter):
+#     #         last_letter = value[-1]
+#     #         word_list.append(value)
+#     #         del name_dictionary[key]
+
+#     # return word_list
 
 #####################################################################
 # You can ignore everything below this.
