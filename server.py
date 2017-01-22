aeda3e91f27aad1e4ec3a0dac63c2c964d3b29b4
Kathryn Rowe <kjrowe06@gmail.com>
2017-01-22 05:21:04 +0000

    Templates, css, and python files set-up. Need internet to complete the rest

---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+import secret_key
+
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = secret_key.secret_key
+
+
+@app.route("/")
+def index():
+    """Return homepage."""
+
+    return render_template("index.html")
+
+
+@app.route("/application-form")
+def application_form():
+    """Sends user to Application Form."""
+    jobs = ["Software Engineer", "QA Engineer", "Product Manager"]
+
+    return render_template("application-form.html", jobs=jobs)
+
+
+@app.route("/application-success")
+def application_success():
+    """Returns a response that acknowledges their application"""
+
+    first_name = request.args.get("firstname")
+    last_name = request.args.get("lastname")
+    applicant = first_name + " " + last_name
+    job_choice = request.args.get()
+    salary_requirement = request.args.get("")
+
+    return render_template("application-response.html",
+                            applicant=applicant,
+                            job_choice=job_choice,
+                            salary_requirement=salary_requirement)
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
