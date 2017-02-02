d47dc2bbc73a4ccbcf9f3afdc2fb9817bacb4de5
Kathryn Rowe <kjrowe06@gmail.com>
2017-02-01 17:59:04 -0800

    We can register user and they can login/logout. YES!

--- server.py
+++ server.py
@@ -26,27 +26,84 @@ def index():
     # a = jsonify([1,3])
     return render_template("homepage.html")
 
+
 @app.route("/users")
 def user_list():
 
     users = User.query.all()
     return render_template("user_list.html", users=users)
 
-@app.route("/register" methods=["GET"])
+
+@app.route("/register", methods=["GET"])
 def show_register_form():
 
     return render_template("register_form.html")
 
-@app.route("/register" methods=["POST"])
+
+@app.route("/register", methods=["POST"])
 def register_process():
 
-    emails = db.sessions.query(User.email).all()
     username = request.form.get("username")
     password = request.form.get("password")
-    if username in emails:
+    age = request.form.get("age")
+    zipcode = request.form.get("zipcode")
+
+    emails = User.query.filter(User.email == username).first()
+
+    if emails is None:
+        email = username
+        new_user = User(email=email,
+                        password=password,
+                        age=age,
+                        zipcode=zipcode)
+
+        db.session.add(new_user)
+
+        db.session.commit()
+        session["logged_in"] = username
+
+        # print "We just create the user"
+        flash("Thank you for registering")
+    else:
+        flash("You've already registered. Please log-in.")
+
+    return redirect("/")
 
 
-    return render_template
+@app.route("/login", methods=["GET"])
+def show_login():
+
+    return render_template("sign_in.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_process():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    emails = User.query.filter(User.email == username).first()
+
+    if emails is None:
+        flash("No such email address. Please register")
+        return redirect('/register')
+
+    elif emails.password != password:
+        flash("Incorrect password.")
+        return redirect("/login")
+    else:
+        session["logged_in"] = username
+        flash("Logged in.")
+        return redirect("/")
+
+
+@app.route("/logout")
+def process_logout():
+    """Log user out."""
+
+    del session["logged_in"]
+    flash("Logged out.")
+    return redirect("/")
 
 
 if __name__ == "__main__":
